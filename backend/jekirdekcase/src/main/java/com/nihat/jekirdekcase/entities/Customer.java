95ccb5870bac10d90c08349794074f3b1cef362d
nhtY <n_hat.yalcin@outlook.com>
2024-09-06 17:50:59 +0300

    create Customer entity class

---
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/entities/Customer.java
@@ -0,0 +1,13 @@
+package com.nihat.jekirdekcase.entities;
+
+import java.time.LocalDateTime;
+
+public class Customer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String region;
+    private LocalDateTime registrationDate;
+    private LocalDateTime updatedAt;
+}
