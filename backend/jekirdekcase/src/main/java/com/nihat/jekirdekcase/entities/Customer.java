4a98d2065c57bb87ba5bdc7c8461cb113fecc351
nhtY <n_hat.yalcin@outlook.com>
2024-09-06 21:11:35 +0300

    annotate the Customer entity for persistence.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/entities/Customer.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/entities/Customer.java
@@ -1,13 +1,39 @@
 package com.nihat.jekirdekcase.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String region;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime registrationDate;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
