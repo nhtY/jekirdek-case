2d5c85d3a2d36d2c3a47425a3418418ce104ab02
nhtY <n_hat.yalcin@outlook.com>
2024-09-06 17:45:50 +0300

    create User entity class.

---
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/entities/User.java
@@ -0,0 +1,14 @@
+package com.nihat.jekirdekcase.entities;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
