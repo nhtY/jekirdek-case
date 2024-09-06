00f94f8e96be6a0bf7cd063cf8e699a296329e8f
nhtY <n_hat.yalcin@outlook.com>
2024-09-06 13:44:49 +0300

    initialize the backend

---
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/JekirdekcaseApplication.java
@@ -0,0 +1,13 @@
+package com.nihat.jekirdekcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekirdekcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekirdekcaseApplication.class, args);
+	}
+
+}
---
+++ backend/jekirdekcase/src/test/java/com/nihat/jekirdekcase/JekirdekcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihat.jekirdekcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekirdekcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
