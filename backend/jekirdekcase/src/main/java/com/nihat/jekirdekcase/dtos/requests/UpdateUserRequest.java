da3fd1e661817bbf8c74aeb4024ddc0a8c9af41b
nhtY <n_hat.yalcin@outlook.com>
2024-09-07 23:15:58 +0300

    refactor UpdateUserRequest DTO.
    - username and email are unique fields, and they might be used to identify a user.
    - Those fields can be updated separately in separate methods.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/requests/UpdateUserRequest.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/requests/UpdateUserRequest.java
@@ -2,8 +2,6 @@ package com.nihat.jekirdekcase.dtos.requests;
 
 public record UpdateUserRequest(
         String firstName,
-        String lastName,
-        String email,
-        String username
+        String lastName
 ) {
 }
