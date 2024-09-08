af6bcd96f0522683c944ea7818290d4733f88a4d
nhtY <n_hat.yalcin@outlook.com>
2024-09-08 13:24:12 +0300

    add reason for error of ResourceNotFoundException in the annotation

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/exceptions/ResourceNotFoundException.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.nihat.jekirdekcase.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
