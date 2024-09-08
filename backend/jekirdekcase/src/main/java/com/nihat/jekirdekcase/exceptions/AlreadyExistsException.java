32b86354e024dd2c9dbecf76e01dd6c54beab53b
nhtY <n_hat.yalcin@outlook.com>
2024-09-08 13:23:13 +0300

    add response status and different constructors in AlreadyExistsException class.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/exceptions/AlreadyExistsException.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/exceptions/AlreadyExistsException.java
@@ -1,7 +1,27 @@
 package com.nihat.jekirdekcase.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Resource already exists")
 public class AlreadyExistsException extends RuntimeException {
     public AlreadyExistsException(String message) {
         super(message);
+    }
+
+    public AlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    public AlreadyExistsException() {
+        super();
+    }
+
+    public AlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
