780438424728f5b22e8195827f420f38b8d7ca63
nhtY <n_hat.yalcin@outlook.com>
2024-09-08 14:01:21 +0300

    refactor date-time areas: Convert String to LocalDateTime.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/responses/CreateUserResponse.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/responses/CreateUserResponse.java
@@ -1,10 +1,12 @@
 package com.nihat.jekirdekcase.dtos.responses;
 
+import java.time.LocalDateTime;
+
 public record CreateUserResponse(
         Long id,
         String firstName,
         String lastName,
         String username,
         String email,
-        String createdAt) {
+        LocalDateTime createdAt) {
 }
