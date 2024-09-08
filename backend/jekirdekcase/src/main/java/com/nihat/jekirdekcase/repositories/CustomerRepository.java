31e1bad85393dcf24a21272dae77229af8c2815e
nhtY <n_hat.yalcin@outlook.com>
2024-09-08 14:09:39 +0300

    add new customer repository method: existsByEmail.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/repositories/CustomerRepository.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/repositories/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.nihat.jekirdekcase.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Boolean existsByEmail(String email);
 }
