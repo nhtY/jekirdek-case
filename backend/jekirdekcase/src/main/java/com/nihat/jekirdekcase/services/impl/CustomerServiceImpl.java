b554d8c6e69fa2fcac1ea8124b57d0c9aa714a19
nhtY <n_hat.yalcin@outlook.com>
2024-09-08 14:36:57 +0300

    implement saveCustomer method.
    - passed the saveCustomer_ShouldReturnCreateCustomerResponse test
    - passed the saveCustomer_ShouldThrowAlreadyExistsExceptionIfEmailExists test.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/services/impl/CustomerServiceImpl.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/services/impl/CustomerServiceImpl.java
@@ -5,10 +5,12 @@ import com.nihat.jekirdekcase.dtos.requests.UpdateCustomerRequest;
 import com.nihat.jekirdekcase.dtos.responses.CreateCustomerResponse;
 import com.nihat.jekirdekcase.dtos.responses.GetCustomerResponse;
 import com.nihat.jekirdekcase.dtos.responses.UpdateCustomerResponse;
+import com.nihat.jekirdekcase.exceptions.AlreadyExistsException;
 import com.nihat.jekirdekcase.mappers.CustomerMapper;
 import com.nihat.jekirdekcase.repositories.CustomerRepository;
 import com.nihat.jekirdekcase.services.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,24 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CreateCustomerResponse saveCustomer(CreateCustomerRequest createCustomerRequest) {
+        try {
+            return customerMapper.mapToCreateCustomerResponse(
+                    customerRepository.save(customerMapper.mapToCustomer(createCustomerRequest))
+            );
+        } catch (DataIntegrityViolationException e) {
+            AlreadyExistsException ex = throwAlreadyExistsExceptionForCustomerCreation(createCustomerRequest.email());
+            if (ex != null) {
+                throw ex;
+            } else {
+                throw  e;
+            }
+        }
+    }
+
+    private AlreadyExistsException throwAlreadyExistsExceptionForCustomerCreation(String email) {
+        if (customerRepository.existsByEmail(email)) {
+            return new AlreadyExistsException("Customer with this email: " + email + " already exists.");
+        }
         return null;
     }
 
