62b7630d5638bf82d08531606e0fc91c1944f372
nhtY <n_hat.yalcin@outlook.com>
2024-09-10 17:24:20 +0300

    add fetch method to get data as a stream in CustomerRepository.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/repositories/CustomerRepository.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/repositories/CustomerRepository.java
@@ -1,8 +1,20 @@
 package com.nihat.jekirdekcase.repositories;
 
 import com.nihat.jekirdekcase.entities.Customer;
+import jakarta.persistence.QueryHint;
+import org.hibernate.jpa.AvailableHints;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import java.util.stream.Stream;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Boolean existsByEmail(String email);
+
+    @Query("SELECT c FROM Customer c")
+    @QueryHints(
+            @QueryHint(name = AvailableHints.HINT_FETCH_SIZE, value = "10")
+    )
+    Stream<Customer> streamAll();
 }
