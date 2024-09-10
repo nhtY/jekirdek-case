ca31a1c8383b817c6dcd66759631b22b6fa0a167
nhtY <n_hat.yalcin@outlook.com>
2024-09-10 17:24:52 +0300

    add two new methods to process stream of data in CustomerService interface.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/services/CustomerService.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/services/CustomerService.java
@@ -7,7 +7,10 @@ import com.nihat.jekirdekcase.dtos.responses.GetCustomerResponse;
 import com.nihat.jekirdekcase.dtos.responses.UpdateCustomerResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.OutputStream;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CustomerService {
@@ -17,4 +20,12 @@ public interface CustomerService {
     GetCustomerResponse getCustomer(Long id);
     List<GetCustomerResponse> getAllCustomers();
     Page<GetCustomerResponse> getAllCustomers(Pageable pageable);
-}
+
+    void streamFilteredCustomers(String firstName, String lastName, String email, String region,
+                                 LocalDate registrationDateStart, LocalDate registrationDateEnd,
+                                 OutputStream outputStream);
+
+    SseEmitter streamFilteredCustomers(String firstName, String lastName, String email, String region,
+                                              LocalDate registrationDateStart, LocalDate registrationDateEnd);
+
+    }
