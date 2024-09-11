e828bb22591a2a1ea348a54bc941819326fbd5a9
nhtY <n_hat.yalcin@outlook.com>
2024-09-11 18:11:58 +0300

    create user dashboard

---
+++ frontend/my-crm-app/src/pages/UserDashboard.jsx
@@ -0,0 +1,15 @@
+import Sidebar from '../components/Sidebar';
+
+const UserDashboard = () => {
+    return (
+        <div className="d-flex">
+            <Sidebar />
+            <div className="p-3">
+                <h2>User Dashboard</h2>
+                {/* Main content will be added later */}
+            </div>
+        </div>
+    );
+};
+
+export default UserDashboard;
