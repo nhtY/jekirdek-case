3715e27b08d5f246e43c1cbe08850020d21af843
nhtY <n_hat.yalcin@outlook.com>
2024-09-11 18:11:38 +0300

    create CreateCustomers page

---
+++ frontend/my-crm-app/src/pages/CreateCustomerPage.jsx
@@ -0,0 +1,11 @@
+
+const CreateCustomerPage = () => {
+    return (
+        <div>
+            <h2>Create Customer</h2>
+            {/* Form will be added later */}
+        </div>
+    );
+};
+
+export default CreateCustomerPage;
