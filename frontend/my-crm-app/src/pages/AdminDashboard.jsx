fe02f4274577afb1e5a4764b9400afe3daf55a9b
nhtY <n_hat.yalcin@outlook.com>
2024-09-11 18:12:09 +0300

    create admin dashboard

---
+++ frontend/my-crm-app/src/pages/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import Sidebar from '../components/Sidebar';
+
+const AdminDashboard = () => {
+    return (
+        <div className="d-flex">
+            <Sidebar />
+            <div className="p-3">
+                <h2>Admin Dashboard</h2>
+                {/* Main content will be added later */}
+            </div>
+        </div>
+    );
+};
+
+export default AdminDashboard;
