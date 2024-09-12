3932bf8a7c2af2cc8663dd1c7066d308baeac7fe
nhtY <n_hat.yalcin@outlook.com>
2024-09-12 23:44:52 +0300

    configure the swagger for authentication.

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/config/SwaggerConfig.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/config/SwaggerConfig.java
@@ -12,6 +12,17 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("public-api")
                 .pathsToMatch("/api/**")
+                .addOpenApiCustomizer(openApi -> openApi
+                        .components(new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes("cookieAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.APIKEY)
+                                        .in(io.swagger.v3.oas.models.security.SecurityScheme.In.COOKIE)
+                                        .name("X-Auth-Token")
+                                )
+                        )
+                        .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("cookieAuth"))
+                )
                 .build();
     }
+
 }
