d2bc80fd0d55571ea1636caf656d0f52d4de4195
nhtY <n_hat.yalcin@outlook.com>
2024-09-12 13:19:12 +0300

    make links in the navbar as dropdown

--- frontend/my-crm-app/src/components/Navbar.jsx
+++ frontend/my-crm-app/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, NavDropdown } from 'react-bootstrap';
 import { useAuth } from '../hooks/useAuth';
 
 const AppNavbar = () => {
@@ -8,7 +8,7 @@ const AppNavbar = () => {
     const [expanded, setExpanded] = useState(false); // State to control collapse behavior
 
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
+        <Navbar bg="dark" variant="dark" expand="sm" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     CompanyName
@@ -17,16 +17,16 @@ const AppNavbar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         {isLoggedIn && role === 'ADMIN' && (
-                            <>
-                                <Nav.Link as={Link} to="/admin/create-user" onClick={() => setExpanded(false)}>Create User</Nav.Link>
-                                <Nav.Link as={Link} to="/admin/list-users" onClick={() => setExpanded(false)}>List Users</Nav.Link>
-                            </>
+                            <NavDropdown title="User Operations" id="admin-dropdown">
+                                <NavDropdown.Item as={Link} to="/admin/create-user" onClick={() => setExpanded(false)}>Create User</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/admin/list-users" onClick={() => setExpanded(false)}>List Users</NavDropdown.Item>
+                            </NavDropdown>
                         )}
                         {isLoggedIn && role === 'USER' && (
-                            <>
-                                <Nav.Link as={Link} to="/user/create-customer" onClick={() => setExpanded(false)}>Create Customer</Nav.Link>
-                                <Nav.Link as={Link} to="/user/list-customers" onClick={() => setExpanded(false)}>List Customers</Nav.Link>
-                            </>
+                            <NavDropdown title="Customer Operations" id="user-dropdown">
+                                <NavDropdown.Item as={Link} to="/user/create-customer" onClick={() => setExpanded(false)}>Create Customer</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/user/list-customers" onClick={() => setExpanded(false)}>List Customers</NavDropdown.Item>
+                            </NavDropdown>
                         )}
                     </Nav>
                     {isLoggedIn && (
