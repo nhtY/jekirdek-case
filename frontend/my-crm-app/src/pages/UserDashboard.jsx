068573ac37b788dccbf5be59cf40d74c6d35c5a4
nhtY <n_hat.yalcin@outlook.com>
2024-09-12 13:22:35 +0300

    use nested routes as Outlet, and use container-fluid for table to have scroll bar

--- frontend/my-crm-app/src/pages/UserDashboard.jsx
+++ frontend/my-crm-app/src/pages/UserDashboard.jsx
@@ -1,12 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 
 const UserDashboard = () => {
     return (
-        <div className="d-flex">
+        <div className="d-flex" style={{ minHeight: '100%' }}>
             <Sidebar />
-            <div className="p-3">
+            <div className="container-fluid container-xxl">
                 <h2>User Dashboard</h2>
-                {/* Main content will be added later */}
+                {/* Main content*/}
+                <Outlet />
             </div>
         </div>
     );
