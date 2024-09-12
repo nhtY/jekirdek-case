da5728f386f14aaadc1b1f7cf78a4625eb405e92
nhtY <n_hat.yalcin@outlook.com>
2024-09-12 13:21:28 +0300

    style change: sidebar will be shown after table view, and has 100% of the height. (its parent has 100vh, so it does)

--- frontend/my-crm-app/src/components/Sidebar.jsx
+++ frontend/my-crm-app/src/components/Sidebar.jsx
@@ -1,14 +1,30 @@
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import { useAuth } from '../hooks/useAuth'; // Import the custom hook
+import { useEffect, useState } from 'react';
 
 const Sidebar = () => {
     const { role, isLoggedIn } = useAuth(); // Get role and isLoggedIn status from AuthContext
 
+    // get if screen is smaller than tablet view
+    const [isTablet, setIsTablet] = useState(window.innerWidth > 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsTablet(window.innerWidth > 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
-            {isLoggedIn && (  // Sidebar should only be visible for logged in users
-                <Navbar bg="light" className="flex-column" expand="lg" style={{ width: '250px' }}>
+            {isLoggedIn && isTablet && (  // Sidebar should only be visible for logged in users
+                <Navbar bg="light" className="flex-column" expand="sm" style={{ minWidth: '180px', minHeight: '100%'}} >
                     <Navbar.Brand href="#">Dashboard</Navbar.Brand>
                     <Nav className="flex-column">
                         {role === 'ADMIN' && (
