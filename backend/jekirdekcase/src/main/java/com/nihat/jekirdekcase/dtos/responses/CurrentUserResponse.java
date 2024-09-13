de28c5ebda3258fa5100ddc010f04ea31d4d7bb6
nhtY <n_hat.yalcin@outlook.com>
2024-09-13 15:09:56 +0300

    refactor

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/responses/CurrentUserResponse.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/dtos/responses/CurrentUserResponse.java
@@ -1,5 +1,6 @@
 package com.nihat.jekirdekcase.dtos.responses;
 
+import java.util.Collection;
 import java.util.Set;
 
-public record CurrentUserResponse(String username, Set<String> authorities) {}
+public record CurrentUserResponse(String username, Collection<String> authorities) {}
