23ff8a8875e4b2506be1c8fe637aeee915d290d0
nhtY <n_hat.yalcin@outlook.com>
2024-09-13 16:18:45 +0300

    fix cors config

--- backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/config/WebConfig.java
+++ backend/jekirdekcase/src/main/java/com/nihat/jekirdekcase/config/WebConfig.java
@@ -9,10 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/") // Update with your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:5173") // Allow requests from your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
+                .allowedHeaders("*") // Allow all headers
+                .exposedHeaders("device-os", "browser") // Expose custom headers
+                .allowCredentials(true); // If using cookies or other credentials
     }
 }
