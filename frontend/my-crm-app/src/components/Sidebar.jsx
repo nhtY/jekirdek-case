048d8707a768192b1d919e49cc854743cc47f0aa
nhtY <n_hat.yalcin@outlook.com>
2024-09-15 00:41:07 +0300

    update sidebar as offcanvas

--- frontend/my-crm-app/src/components/Sidebar.jsx
+++ frontend/my-crm-app/src/components/Sidebar.jsx
@@ -1,51 +1,123 @@
-import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
-import { useAuth } from '../hooks/useAuth'; // Import the custom hook
-import { useEffect, useState } from 'react';
+import { Nav } from "react-bootstrap";
+import { useAuth } from "../hooks/useAuth"; // Import the custom hook
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFilter, faList, faPlusCircle, faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    const { role, isLoggedIn } = useAuth(); // Get role and isLoggedIn status from AuthContext
+  const { role, isLoggedIn, logout } = useAuth(); // Get role and isLoggedIn status from AuthContext
+  console.log(role, isLoggedIn);
 
-    // get if screen is smaller than tablet view
-    const [isTablet, setIsTablet] = useState(window.innerWidth > 768);
+  const handleLogout = () => {
+    logout();
+  };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsTablet(window.innerWidth > 768);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      // setIsTablet(window.innerWidth > 768);
+    };
 
-        window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    return (
-        <>
-            {isLoggedIn && isTablet && (  // Sidebar should only be visible for logged in users
-                <Navbar bg="light" className="flex-column" expand="sm" style={{ minWidth: '180px', minHeight: '100%'}} >
-                    <Navbar.Brand href="#">Dashboard</Navbar.Brand>
-                    <Nav className="flex-column">
-                        {role === 'ADMIN' && (
-                            <>
-                                <Nav.Link as={Link} to="/admin/create-user">Create User</Nav.Link>
-                                <Nav.Link as={Link} to="/admin/list-users">List Users</Nav.Link>
-                            </>
-                        )}
-                        {role === 'USER' && (
-                            <>
-                                <Nav.Link as={Link} to="/user/create-customer">Create Customer</Nav.Link>
-                                <Nav.Link as={Link} to="/user/list-customers">List Customers</Nav.Link>
-                                <Nav.Link as={Link} to="/user/filter-customers-with-specs">Filter With Specs</Nav.Link>
-                            </>
-                        )}
-                    </Nav>
-                    <div className='mt-auto'></div>
-                </Navbar>
+  return (
+    <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+      <div
+        className="offcanvas-md offcanvas-end bg-body-tertiary"
+        tabIndex="-1"
+        id="sidebarMenu"
+        aria-labelledby="sidebarMenuLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="sidebarMenuLabel">
+            Company name
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            data-bs-target="#sidebarMenu"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+          <ul className="nav flex-column">
+            {role === "ROLE_ADMIN" && (
+              <>
+                <Nav.Link
+                  as={Link}
+                  to="/admin/create-user"
+                  className="d-flex align-items-center gap-2"
+                >
+                  <FontAwesomeIcon icon={faPlusCircle} />
+                  Create User
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to="/admin/list-users"
+                  className="d-flex align-items-center gap-2"
+                >
+                  List Users
+                </Nav.Link>
+              </>
             )}
-        </>
-    );
+            {role === "ROLE_USER" && (
+              <>
+                <Nav.Link
+                  as={Link}
+                  to="/user/create-customer"
+                  className="d-flex align-items-center gap-2"
+                >
+                    <FontAwesomeIcon icon={faPlusCircle} />
+                  Create Customer
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to="/user/list-customers"
+                  className="d-flex align-items-center gap-2"
+                >
+                    <FontAwesomeIcon icon={faList} />
+                  List Customers
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to="/user/filter-customers-with-stream"
+                  className="d-flex align-items-center gap-2"
+                >
+                    <FontAwesomeIcon icon={faFilter} />
+                  Filter With Stream
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to="/user/filter-customers-with-specs"
+                  className="d-flex align-items-center gap-2"
+                >
+                    <FontAwesomeIcon icon={faFilter} />
+                  Filter With JPA Specs
+                </Nav.Link>
+              </>
+            )}
+            <hr className="my-3" />
+            <li className="nav-item">
+              <Nav.Link
+                as={Link}
+                className="d-flex align-items-center gap-2"
+                onClick={() => handleLogout()}
+              >
+                <FontAwesomeIcon icon={faSignOut} />
+                Logout
+              </Nav.Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
