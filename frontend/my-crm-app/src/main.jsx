3eba6442e134983c6f4791cc7d03632cc261e93f
nhtY <n_hat.yalcin@outlook.com>
2024-09-15 00:38:31 +0300

    import bootstrap.js for data-bs to work

--- frontend/my-crm-app/src/main.jsx
+++ frontend/my-crm-app/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.esm.js'
 
 
 createRoot(document.getElementById('root')).render(
