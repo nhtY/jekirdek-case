99aa7e37eb1ec8d7732fc5b22c78455990099fcd
nhtY <n_hat.yalcin@outlook.com>
2024-09-16 00:07:16 +0300

    update auth context to connect to backend

--- frontend/my-crm-app/src/context/AuthContext.jsx
+++ frontend/my-crm-app/src/context/AuthContext.jsx
@@ -1,37 +1,62 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import axios from '../axios/axios'; // Update this with your actual axios instance path
+import { useNavigate } from 'react-router-dom';
 
 // Create Auth Context
 export const AuthContext = createContext(null);
 
-
-
-// eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
     const [authData, setAuthData] = useState({
         user: null,
         role: '',
-        token: '',
         isLoggedIn: false,
     });
+    const navigate = useNavigate();
+
+    // Fetch current user from backend when app loads
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            try {
+                const response = await axios.get('/auth/current-user');
+                setAuthData({
+                    user: response.data.username,
+                    role: response.data.authorities[0],
+                    isLoggedIn: true,
+                });
+            } catch (error) {
+                setAuthData({
+                    user: null,
+                    role: '',
+                    isLoggedIn: false,
+                });
+                navigate('/'); // Navigate to login page if not authenticated
+            }
+        };
+        fetchCurrentUser();
+    }, [navigate]);
 
     // Login function
-    const login = (user, role, token) => {
+    const login = (user, role) => {
         setAuthData({
             user,
             role,
-            token,
             isLoggedIn: true,
         });
     };
 
     // Logout function
-    const logout = () => {
-        setAuthData({
-            user: null,
-            role: '',
-            token: '',
-            isLoggedIn: false,
-        });
+    const logout = async () => {
+        try {
+            await axios.post('/auth/logout');
+            setAuthData({
+                user: null,
+                role: '',
+                isLoggedIn: false,
+            });
+            navigate('/');
+        } catch (error) {
+            console.error('Failed to logout:', error);
+        }
     };
 
     return (
