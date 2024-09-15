5469f996ebeeb493c5073e0e6b83a3766b350a48
nhtY <n_hat.yalcin@outlook.com>
2024-09-16 00:04:59 +0300

    wrap the app with Router to be able to utilize useNavigate hook in anywhere

--- frontend/my-crm-app/src/App.jsx
+++ frontend/my-crm-app/src/App.jsx
@@ -1,37 +1,27 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppNavbar from './components/Navbar';
-import Footer from './components/Footer';
-import AppRoutes from './routes';
-import AuthProvider from './context/AuthContext';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppNavbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AppRoutes from "./routes";
+import AuthProvider from "./context/AuthContext";
 
 const App = () => {
-    return (
-        <AuthProvider>
-            <Router>
-                
-                <AppNavbar />
-                <div className='container-fluid'>
-                    <AppRoutes />
-                </div>
-                <footer className='sticky-bottom'>
-                    <Footer />
-                </footer>
-                {/* <div className="d-flex flex-column min-vh-100">
-                    <AppNavbar />
-                    <main className="vh-100">
-                        <AppRoutes />
-                        <Footer />
-                    </main>
-                    
-                </div> */}
-            </Router>
-        </AuthProvider>
-    );
+  return (
+    <Router>
+      <AuthProvider>
+        <AppNavbar />
+        <div className="container-fluid">
+          <AppRoutes />
+        </div>
+        <footer className="">
+          <Footer />
+        </footer>
+      </AuthProvider>
+    </Router>
+  );
 };
 
 export default App;
-
