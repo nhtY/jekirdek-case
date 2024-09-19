b814abd9f8148cd7431154f7fb374d961c1600b2
nhtY <n_hat.yalcin@outlook.com>
2024-09-19 16:14:59 +0300

    wrap the app with error provider

--- frontend/my-crm-app/src/App.jsx
+++ frontend/my-crm-app/src/App.jsx
@@ -7,20 +7,25 @@ import AppNavbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes";
 import AuthProvider from "./context/AuthContext";
+import { ErrorProvider } from "./context/ErrorContext";
+import ErrorModal from "./components/ErrorModal";
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <AppNavbar />
-        <div className="container-fluid">
-          <AppRoutes />
-        </div>
-        <footer className="">
-          <Footer />
-        </footer>
-      </AuthProvider>
-    </Router>
+    <ErrorProvider>
+      <Router>
+        <AuthProvider>
+          <AppNavbar />
+          <div className="container-fluid">
+            <AppRoutes />
+          </div>
+          <footer className="">
+            <Footer />
+          </footer>
+          <ErrorModal />
+        </AuthProvider>
+      </Router>
+    </ErrorProvider>
   );
 };
 
